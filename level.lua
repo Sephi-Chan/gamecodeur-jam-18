f7e0344ecaabcf8164528e93113617750532cd8c
Sephi-Chan <tribes.romain@gmail.com>
2018-11-12 01:02:40 +0100

    Fix camera bugs.

--- level.lua
+++ level.lua
@@ -4,13 +4,13 @@ local Level = {}
 function Level.one(camera, hero)
     local front_herbs           = Layer.new(love.graphics.newImage("images/layers/10-front-herbs.png"), 1)
     local road                  = Layer.new(love.graphics.newImage("images/layers/20-road.png"), 1)
-    local front_trees           = Layer.new(love.graphics.newImage("images/layers/30-front-trees.png"), 1)
-    local background_herbs      = Layer.new(love.graphics.newImage("images/layers/40-background-herbs.png"), 1)
-    local background_trees      = Layer.new(love.graphics.newImage("images/layers/50-background-trees.png"), 1)
-    local more_background_herbs = Layer.new(love.graphics.newImage("images/layers/60-more-background-herbs.png"), 1)
-    local sky_background_top    = Layer.new(love.graphics.newImage("images/layers/70-sky-background-top.png"), 1)
-    local more_background_trees = Layer.new(love.graphics.newImage("images/layers/80-more-background-trees.png"), 1)
-    local sky_background        = Layer.new(love.graphics.newImage("images/layers/90-sky-background.png"), 1)
+    local front_trees           = Layer.new(love.graphics.newImage("images/layers/30-front-trees.png"), 3)
+    local background_herbs      = Layer.new(love.graphics.newImage("images/layers/40-background-herbs.png"), 4)
+    local background_trees      = Layer.new(love.graphics.newImage("images/layers/50-background-trees.png"), 5)
+    local more_background_herbs = Layer.new(love.graphics.newImage("images/layers/60-more-background-herbs.png"), 6)
+    local sky_background_top    = Layer.new(love.graphics.newImage("images/layers/70-sky-background-top.png"), 7)
+    local more_background_trees = Layer.new(love.graphics.newImage("images/layers/80-more-background-trees.png"), 8)
+    local sky_background        = Layer.new(love.graphics.newImage("images/layers/90-sky-background.png"), 9)
      
     local level = {
       width  = 8000,
@@ -38,8 +38,8 @@ function Level.one(camera, hero)
     Camera.attach(camera, 5, background_trees)
     Camera.attach(camera, 4, background_herbs)
     Camera.attach(camera, 3, front_trees)
-    Camera.attach(camera, 2, road)
-    Camera.attach(camera, 1.5, function() Entity.draw(Entity.sortByY(Entity.entities())) end)
+    Camera.attach(camera, 1, function() Entity.draw(Entity.sortByY(Entity.entities())) end)
+    Camera.attach(camera, 1, road)
     Camera.attach(camera, 1, front_herbs)
 
     return level
