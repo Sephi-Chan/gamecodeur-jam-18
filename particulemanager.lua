84646f035ec4742b126d3c7793b07c4f7f9e9515
Sephi-Chan <tribes.romain@gmail.com>
2018-11-17 14:12:47 +0100

    Draw the particles with the right color.

--- particulemanager.lua
+++ particulemanager.lua
@@ -15,18 +15,19 @@ function Particule_Manager.update(particule_manager, delta)
       particule_manager.effects_active[i].effect.system:update(delta*factor)
     end
   end
-  
+
   Particule_Manager.remove_efficient(particule_manager.effects_active,particule_effect_to_remove )
-  
+
 end
 
 function Particule_Manager.draw(particule_manager)
+  love.graphics.setColor(1, 1, 1)
   for i=1, #particule_manager.effects_active do
     local position = { x =  particule_manager.effects_active[i].object_to_draw_on.x, y =particule_manager.effects_active[i].object_to_draw_on.y}
      love.graphics.draw(particule_manager.effects_active[i].effect.system, position.x -camera.x, position.y -camera.y)
   end
-  
-  
+
+
 end
 
 
@@ -38,9 +39,9 @@ function Particule_Manager.add_particule_effect(particule_manager, particule_eff
     active_system.effect = Particule_Manager.create_new_blood_particule()
   end
 
-  
+
   active_system.object_to_draw_on = object_to_draw_on
-  
+
   table.insert(particule_manager.effects_active, active_system)
 end
 
@@ -49,7 +50,7 @@ function Particule_Manager.create_new_health_particule()
   health_particule.img = love.graphics.newImage("images/health_particule.png")
   health_particule.timer= 0
   health_particule.duration= 3
-  
+
   health_particule.system = love.graphics.newParticleSystem(health_particule.img, 32)
   health_particule.system:setParticleLifetime(1, 2) -- Particles live at least 2s and at most 5s.
   health_particule.system:setEmissionArea( "normal", 20, 20, 0, false )
@@ -57,31 +58,31 @@ function Particule_Manager.create_new_health_particule()
   health_particule.system:setEmitterLifetime( 1 )
 	health_particule.system:setEmissionRate(10)
 	health_particule.system:setSizeVariation(1)
-  
+
 	health_particule.system:setLinearAcceleration(0, -40, 0, -40) -- Random movement in all directions.
   health_particule.system:setSizes(0.4)
 	health_particule.system:setColors(255, 255, 255, 255, 255, 255, 255, 0) -- Fade to transparency.
-  
-  
-  
+
+
+
   return health_particule
 end
 
 function Particule_Manager.create_new_blood_particule()
-  
+
    local blood_particule = {}
    blood_particule.img = love.graphics.newImage("images/blood_particule.png")
    blood_particule.timer= 0
    blood_particule.duration= 2
-  
+
    blood_particule.system = love.graphics.newParticleSystem(blood_particule.img, 32)
    blood_particule.system:setSizes(0.5)
    blood_particule.system:setTangentialAcceleration(100,100)
    blood_particule.system:setDirection( math.rad(-50) )
    blood_particule.system:setSpeed(120,150)
    blood_particule.system:setLinearAcceleration(-5, 80, -5, 80)
-   
-   
+
+
    blood_particule.system:setParticleLifetime(1, 0.5)
    blood_particule.system:setPosition( 10, -20 )
    blood_particule.system:setEmitterLifetime( 1 )
@@ -90,7 +91,7 @@ function Particule_Manager.create_new_blood_particule()
    blood_particule.system:setEmissionRate(7)
    blood_particule.system:setRadialAcceleration(15,30)
    blood_particule.system:setColors(255, 255, 255, 255, 0, 0, 0, 0) -- Fade to transparency.
-  
+
   return blood_particule
 end
 
@@ -119,9 +120,9 @@ end
 for i=j+1,n do
   input[i]=nil
 end
-  
+
 end
-  
+
 
 function Particule_Manager:initialize()
   local new_particule_manager = {}
